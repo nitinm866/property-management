f79f963c7b35ab1584a5f7de68b90379750376b3
nitinm866 <nitins42@yahoo.com>
2025-01-25 20:19:24 +0530

    Initial commit

---
+++ src/test/java/com/nitin/property_management/PropertyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.property_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
